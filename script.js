b0b048198e574f9025d3a326e2f0d619c7452bc0
unknown <rob_kriv@hotmail.com>
2019-01-16 13:58:09 -0700

    added js stuff

---
+++ script.js
@@ -0,0 +1,33 @@
+//script.js
+
+
+$.getJSON('https://api.unsplash.com/users/linussandvide/photos/?client_id=b6c05bdd32868cb9dc6c23802dc9c8ad665422186d488ffce0af7bf860004ec7', function(data) {console.log(data);
+
+$.each(data, function(index, value) {
+    console.log(value);
+	
+	var swag = value.user.
+	
+	
+   });
+});
+
+
+/* $.ajax({
+  dataType: "json",
+  url: https://api.unsplash.com/users/brianhaferkamp/photos/?client_id=b6c05bdd32868cb9dc6c23802dc9c8ad665422186d488ffce0af7bf860004ec7',
+  data: data,
+  success: success
+});
+ */
+ 
+/*
+
+*/
+
+$(function(){
+	alert("good job!");	
+});
+
+//b6c05bdd32868cb9dc6c23802dc9c8ad665422186d488ffce0af7bf860004ec7
+//linussandvide
